904e0998df28ba776a94f6b0b9376ddf78bab883
CJ-SA <carlosdsansilva@gmail.com>
2026-09-01 14:35:35 -0300

    nível 2, exercicio 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,22 @@
 package org.example;
-
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main()
-    {
-        int A= 20;
-        int B= 21;
-
-        System.out.println( "A é igual a B? "+ ( A == B ) +"\nA é menor a B? " + (A<B) +"\nA é difrente a B? " + (A!=B) + "\nA é maior que B? " + (A>B) + "\nA é maior ou igual que B? " + (A >= B) + "\nA é menor ou igual que B? " + (A<=B));
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("por favor, diga sua nota entre 0 a 100:");
+        int nota = sc.nextInt();
+        System.out.println("agora fale sua frequência em porcentagem:");
+        int freq = sc.nextInt();
+        boolean cond1 = (nota >= 80) && (freq >= 75);
+        boolean cond2 = ((nota >= 50) && (nota <= 80)) || (freq >= 75);
+        if (!cond2 && !cond1) {
+            System.out.println("reprovado");
+        } else if (cond1) {
+            System.out.println("aprovado com B");
+        } else if (cond2) {
+            System.out.println("aprovado com R");
+        }
     }
 }
