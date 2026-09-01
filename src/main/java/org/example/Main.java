aee7f42b8749e6f248f3995457cc1776347f7fe3
CJ-SA <carlosdsansilva@gmail.com>
2026-09-01 14:47:14 -0300

    nível 5, exercicio 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,20 +7,16 @@ public class Main {
         Scanner sc = new Scanner (System.in);
 
 
-        System.out.println("digite o valor de A: ");
-        double n1 = sc.nextDouble ();
-        double n3 = n1;
+        System.out.print("digite seu peso [kg]: ");
+        double peso = sc.nextDouble ();
 
 
-        System.out.println("digite o valor de B: ");
-        double n2 = sc.nextDouble ();
+
+        System.out.print("digite sua altura [m]: ");
+        double alt = sc.nextDouble ();
 
 
-        n1 = n2;
-        System.out.println("O valor de A foi atualizado para o valor de B: " + n1);
-        n2 = n3;
-
-
-        System.out.println("O valor de B foi atualizado para o valor de A: " + n2);
+        double IMC = peso / (alt * alt);
+        System.out.println("Seu IMC é: " + IMC);
     }
 }
